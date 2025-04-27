fbbb8edd270d633d91cc011063301109fa6262dc
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-04-27 17:16:44 +0300

    update Kotlin and KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.9.2" apply false
     id("com.android.library") version "8.9.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.54" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
 }
